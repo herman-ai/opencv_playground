2024e50187b10431e5f56798434b72d736c44fff
HS <herman.sahota@gmail.com>
2019-09-19 18:10:47 -0700

    Initial Commit

---
+++ opencv_playground/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencv_playground
+//
+//  Created by HS on 9/19/19.
+//  Copyright © 2019 HS. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
