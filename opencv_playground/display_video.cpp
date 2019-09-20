9f517c730d74ddd669a3529891f61ba8ddfc1502
HS <herman.sahota@gmail.com>
2019-09-20 13:57:12 -0700

    Add controls for video playback.

--- opencv_playground/display_video.cpp
+++ opencv_playground/display_video.cpp
@@ -15,33 +15,76 @@
 using namespace cv;
 using namespace std;
 
+int g_slider_position = 0;
+int g_run = 1, g_dontset = 0;
+
+VideoCapture g_cap;
+
+void onTrackBarSlide(int pos, void *) {
+    g_cap.set(CAP_PROP_POS_FRAMES, pos);
+    if (!g_dontset) {
+        g_run = 1;
+    }
+    g_dontset = 0;
+}
+
 int display_video() {
     
-    VideoCapture cap;
     string fname;
+    bool done = false;
     
     cout << "Enter the path to the video file: ";
     cin >> fname;
     
-    namedWindow("Showing video");
+    namedWindow("VIDEO", WINDOW_AUTOSIZE);
     
-    cap.open(fname);
+    
+    g_cap.open(fname);
+    
+    int frames = (int) g_cap.get(CAP_PROP_FRAME_COUNT);
+    int tmpw = (int) g_cap.get(CAP_PROP_FRAME_WIDTH);
+    int tmph = (int) g_cap.get(CAP_PROP_FRAME_HEIGHT);
+    
+    createTrackbar("Position", "VIDEO", &g_slider_position, frames, onTrackBarSlide);
+    
+    cout << "Number of frames = " << frames << endl;
+    cout << "Width of each frame = " << tmpw << endl;
+    cout << "Height of each frame = " << tmph << endl;
+    
     
     Mat frame;
-    int i = 0;
-    while (true) {
-        cap >> frame;
-        cout << "Displaying frame [" << i++ << "]." << endl;
-        if (frame.empty()) {
-            cout << "Video ended" << endl;
-            break;
+
+    while (!done) {
+        if (g_run != 0) {
+            g_cap >> frame;
+            int pos = (int)g_cap.get(CAP_PROP_POS_FRAMES);
+            cout << "Displaying frame [" << pos << "]." << endl;
+            if (frame.empty()) {
+                cout << "Video ended" << endl;
+                break;
+            }
+            setTrackbarPos("Position", "VIDEO", pos);
+            g_run--;
+            g_dontset = 1;
         }
-        imshow("Showing video", frame);
-        if (waitKey(33) >=0) { // Let's assume a frame rate of 30 FPS
-            cout << "User interrupted" << endl;
-            break;
+        imshow("VIDEO", frame);
+        char ch = waitKey(33);
+        ch = tolower(ch);
+        switch (ch) {
+            case 's' :
+                cout << "Single stepping through the video" << endl;
+                g_run = 1;
+                break;
+            case 'r':
+                g_run = -1;
+                cout << "Run mode" << endl;
+                break;
+            case 27:    //escape key
+                cout << "User wants to stop video" << endl;
+                done = true;
+                break;
         }
     }
-    destroyWindow("Showing video");
+    destroyWindow("VIDEO");
     return 0;
 }
