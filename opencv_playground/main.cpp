877806e2a316c621f85ab711c64dabe2239b9d11
HS <herman.sahota@gmail.com>
2019-09-19 19:05:54 -0700

    Add example to read and display an image.

--- opencv_playground/main.cpp
+++ opencv_playground/main.cpp
@@ -7,9 +7,32 @@
 //
 
 #include <iostream>
+#include "opencv2/highgui/highgui.hpp"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+/*
+ This program displays an image loaded from the disk.
+ */
+
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        cout << "Fewer args than expected" << endl;
+        return -1;
+    }
+    Mat image = imread(argv[1], 0);
+    if (image.empty()) {
+        cout << "Could not load the image" << endl;
+        return -1;
+    }
+    
+    //create a gui window:
+    namedWindow("Window name", WINDOW_AUTOSIZE);
+    imshow("Window name", image);
+    
+    waitKey(0);
+    destroyWindow("Window name");
     return 0;
 }
